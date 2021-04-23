e531f9c4770f5a5e737aa947fdd29b75c9b54114
Doruzzz <44002080+Doruzzz@users.noreply.github.com>
2021-04-23 11:06:27 +0200

    Add files via upload

---
+++ PlayerController Prototype 1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 20;
+    private float turnspeed = 45f;
+    private float horizontalinput;
+    private float forwardinput;
+
+    void Update()
+    {
+        horizontalinput = Input.GetAxis("Horizontal");
+        forwardinput = Input.GetAxis("Vertical");
+
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardinput);
+        transform.Rotate(Vector3.up, turnspeed * horizontalinput * Time.deltaTime);
+
+
+    }
+}
